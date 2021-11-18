4bb08f44122c2fd12eafb65a7b70d5e7358750b8
Facundo Saracco <facundosaracco@gmail.com>
2021-11-18 09:24:10 +0100

    almost

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,8 +1,29 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
+function domUpdateAllChar(characters){
+  const charContainer = document.querySelector("div.characters-container")
+  charContainer.innerHTML = ""
+  for (const character of characters){
+    const html = `<div class="characters-container">
+      <div class="character-info">
+        <div class="name">${character.name}</div>
+        <div class="occupation">${character.occupation}</div>
+        <div class="cartoon">Is a Cartoon? ${character.cartoon}</div>
+        <div class="weapon">${character.weapon}</div>
+      </div>
+    </div>`
+    const charCard = document.createElement("div")
+    charCard.classList.add(character-info)
+    charCard.innerHTML = html
+    charContainer.appendChild(charCard)
+  }
+}
+
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    APIHandler.getFullList()
+    .then(characters => domUpdateAllChar(characters.data))
+    .catch(console.log)
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
@@ -18,6 +39,17 @@ window.addEventListener('load', () => {
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log("form submit: ", document.forms[0].elements[0].value)
+
+    const name = document.forms[0].elements[0].value
+    const occupation = document.forms[0].elements[1].value
+    const weapon = document.forms[0].elements[2].value
+    const cartoon = document.forms[0].elements[3].value
+
+    console.log({name, occupation, weapon, cartoon})
+    charactersAPI.createOneRegister({name, occupation, weapon, cartoon})
+    // .then(()=>location.reload())
 
   });
 });
